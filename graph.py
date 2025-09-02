6c40320d1d5265fb7c06bf059a32c5c376078ae1
ddresios <demetrios.dresios@gmail.com>
2025-09-02 14:23:40 -0700

    Removed unused files

--- graph.py
+++
@@ -1,2 +0,0 @@
-#use matplotlib to take the list of coordinates and their qualities and graph them with a heatmap.
-#it would also be nice to include the outline of the US but idk how I'll do that yet
