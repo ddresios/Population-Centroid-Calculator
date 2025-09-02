f868b38a498961e453a875380e6d9c01bc315004
ddresios <demetrios.dresios@gmail.com>
2025-09-02 10:27:10 -0700

    Outlined everything

---
+++ calculations.py
@@ -0,0 +1,7 @@
+#This will be the file where I implement the calculation methods. 
+
+#Method 1: do the haversine formula with the inputs of: a techer's coordinates, and a centroid candidate location's coordinates
+
+#Method 2: Calculate the quality of the centroid by averageing/summing all the distances (not sure yet whether sum or average is better)
+
+#Method 3: take a list with all the possible centroid coordinate candidates, and append to each coordinate the "quality" of the centroid
---
+++ graph.py
@@ -0,0 +1,2 @@
+#use matplotlib to take the list of coordinates and their qualities and graph them with a heatmap.
+#it would also be nice to include the outline of the US but idk how I'll do that yet
